5df85c6e918dae99672e03ea671cc581b57fc941
UAA <uaa04@yandex.ru>
2023-09-01 18:36:50 +0900

    fix

--- hw2/2.py
+++ hw2/2.py
@@ -5,14 +5,14 @@
 from fractions import Fraction
 
 
-def user_input(num_string):
+def User_input(num_string):
     while True:
         user_str = input(f"введите строку {num_string} вида 'a/b' - дробь с числителем и знаменателем\n").split('/')
         if len(user_str) == 2 and user_str[0].isdigit() and user_str[1].isdigit():
             return int(user_str[0]), int(user_str[1])
 
 
-def reduction(a, b):
+def Reduction(a, b):
     """сокращение дроби"""
     fnd = a if a < b else b
     while fnd > 0:
@@ -23,23 +23,23 @@ def reduction(a, b):
     return [int(a), int(b)]
 
 
-def sum_fr(a1, b1, a2, b2):
+def Sum_fr(a1, b1, a2, b2):
     b = b1 * b2
     a = a1 * (b / b1) + a2 * (b / b2)
-    return reduction(a, b)
+    return Reduction(a, b)
 
 
-def mul_fr(a1, b1, a2, b2):
-    return reduction(a1 * a2, b1 * b2)
+def Mul_fr(a1, b1, a2, b2):
+    return Reduction(a1 * a2, b1 * b2)
 
 
-a1, b1 = user_input('1')
+a1, b1 = User_input('1')
 fract1 = Fraction(a1, b1)
-a2, b2 = user_input('2')
+a2, b2 = User_input('2')
 fract2 = Fraction(a2, b2)
 
-result_sum = sum_fr(a1, b1, a2, b2)
-result_mul = mul_fr(a1, b1, a2, b2)
+result_sum = Sum_fr(a1, b1, a2, b2)
+result_mul = Mul_fr(a1, b1, a2, b2)
 
 print()
 print(f"{a1}/{b1} + {a2}/{b2} = {result_sum[0]}{'' if result_sum[1] == 1 else f'/{result_sum[1]}'}")
